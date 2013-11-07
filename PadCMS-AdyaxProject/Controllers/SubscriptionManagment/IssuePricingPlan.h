11ec6cf135deb3089a3d3df8e522cbbe2a6a1cc4
Taras Palienko <palienko.t@dbbest.com>
2013-11-07 19:07:02 +0200

    added issue pricing plan type

---
+++ PadCMS-AdyaxProject/Controllers/SubscriptionManagment/IssuePricingPlan.h
@@ -0,0 +1,16 @@
+//
+//  IssuePricingPlan.h
+//  Pad CMS
+//
+//  Created by Maksym Martyniuk on 11/7/13.
+//  Copyright (c) 2013 Adyax. All rights reserved.
+//
+
+typedef enum{
+    
+    IssuePricingPlanFree,
+    IssuePricingPlanSubscriptionOnly,
+    IssuePricingPlanSubscriptionOrSinglePurchase,
+    IssuePricingPlanSinglePurchase
+    
+}IssuePricingPlan;
