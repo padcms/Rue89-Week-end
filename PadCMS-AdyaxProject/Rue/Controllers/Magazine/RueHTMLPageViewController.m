328e3e6663aad031591e9da94aa516e0c15cd3b8
Taras Palienko <palienko.t@dbbest.com>
2014-01-17 13:01:22 +0200

    html page bug

--- PadCMS-AdyaxProject/Rue/Controllers/Magazine/RueHTMLPageViewController.m
+++ PadCMS-AdyaxProject/Rue/Controllers/Magazine/RueHTMLPageViewController.m
@@ -11,6 +11,7 @@
 #import "PCPageTemplatesPool.h"
 #import "ZipArchive.h"
 #import "PCPageElemetTypes.h"
+#import "PCPageViewController+IsPresented.h"
 
 @interface PCPageViewController ()
 
@@ -25,7 +26,7 @@
     PCLandscapeViewController* _webViewController;
 }
 
-//- (void)showBrowser;
+- (void) showBrowser;
 //- (void)hideBrowser;
 
 @end
@@ -33,6 +34,7 @@
 @interface RueHTMLPageViewController ()
 {
     UIWebView* _webView;
+    NSURL* _URL;
 }
 
 @end
@@ -48,14 +50,9 @@
 {
     [super loadFullView];
     
-    //[NSURLCache setSharedURLCache:[[NSURLCache alloc]initWithMemoryCapacity:15 diskCapacity:30 diskPath:@"customCache"]];
-    
-    //[[NSURLCache sharedURLCache]setMemoryCapacity:5000];
-    
-    if (_webViewController == nil) {
+    if (_webViewController == nil)
+    {
         PCPageElementHtml *pageElement =(PCPageElementHtml *)[self.page firstElementForType:PCPageElementTypeHtml];
-        
-//        NSString* str = nil;
         
         NSURL *url = nil;
         if (pageElement.htmlUrl != nil) {
@@ -75,12 +72,10 @@
                 url = [NSURL fileURLWithPath:[outputDirectory stringByAppendingPathComponent:@"index.html"]];
                 
                 [zipArchive UnzipCloseFile];
-                
-//                str = [NSString stringWithContentsOfFile:[outputDirectory stringByAppendingPathComponent:@"index.html"] encoding:NSUTF8StringEncoding error:nil];
             }
-            
-            //[zipArchive release];
         }
+        
+        _URL = url;
         
         _webViewController = [[PCLandscapeViewController alloc] init];
         
@@ -92,78 +87,59 @@
             webViewRect = CGRectMake(0, 0, 1024, 786);
         }
         
-        UIWebView* webView = [[UIWebView alloc] initWithFrame:webViewRect];
-        webView.backgroundColor = [UIColor blackColor];
-        [webView setDelegate:self];
-        _webView = webView;
-        [_webViewController.view addSubview:webView];
-        
         UIActivityIndicatorView* activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-        [activityView setFrame:CGRectMake((webView.frame.size.width-activityView.frame.size.width)/2, (webView.frame.size.height-activityView.frame.size.height)/2, activityView.frame.size.width, activityView.frame.size.height)];
+        [activityView setFrame:CGRectMake((webViewRect.size.width-activityView.frame.size.width)/2, (webViewRect.size.height-activityView.frame.size.height)/2, activityView.frame.size.width, activityView.frame.size.height)];
         [activityView setTag:100];
         [activityView startAnimating];
         [_webViewController.view addSubview:activityView];
-        //[activityView release];
-        
-        //url = [NSURL fileURLWithPath:@"URL: file:///Users/maks/Library/Application%20Support/iPhone%20Simulator/7.0/Applications/B9B0D571-C2A7-4F53-A665-CB1F90D02A15/Library/PrivateDocuments/application-104/issue-506/revision-623/element/00/01/02/60/resource/diapo.html?v=0.03"];
-        
-        NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:2147483647];
-        //        NSURLRequest* request = [NSURLRequest requestWithURL:url];
-        //request.cachePolicy = NSURLRequestReloadIgnoringCacheData;
-        
-        request.HTTPShouldHandleCookies = NO;
-        
-//        [webView loadHTMLString:str baseURL:nil];
-        
-        [webView loadRequest:request];
-        
-        //[webView release];
-        
-//        double delayInSeconds = 6.0;
-//        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-//        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-//            
-//            [self removeContentFromDisk];
-//            [[NSURLCache sharedURLCache] removeAllCachedResponses];
-//            [webView reload];
-//        });
         
         
+        [self loadWebView];
     }
+    if(_webView == nil)
+    {
+        [self loadWebView];
+    }
+}
+
+- (void) loadWebView
+{
+    CGRect      webViewRect;
+    
+    if (self.columnViewController.horizontalOrientation) {
+        webViewRect = CGRectMake(0, 0, 768, 1024);
+    } else {
+        webViewRect = CGRectMake(0, 0, 1024, 786);
+    }
+    
+    UIWebView* webView = [[UIWebView alloc] initWithFrame:webViewRect];
+    webView.backgroundColor = [UIColor blackColor];
+    [webView setDelegate:self];
+    _webView = webView;
+    
+    [_webViewController.view addSubview:webView];
+    
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:_URL cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:2147483647];
+
+    request.cachePolicy = NSURLRequestReloadIgnoringCacheData;
+    
+    request.HTTPShouldHandleCookies = NO;
+    
+    [webView loadRequest:request];
 }
 
 - (BOOL) webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
     NSLog(@"loading request : %@", request.debugDescription);
-    NSLog(@"request headers : %@", request.allHTTPHeaderFields);
-//    static NSURLRequest* currentRequest = nil;
-//    
-//    if(currentRequest)
-//    {
-//        if([currentRequest.URL.absoluteString isEqualToString:request.URL.absoluteString])
-//        {
-//            return YES;
-//        }
-//        else
-//        {
-//            [webView stopLoading];
-//            NSMutableURLRequest* newRequest = [NSMutableURLRequest requestWithURL:request.URL cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval:5];
-//            
-//            currentRequest = newRequest;
-//            [webView loadRequest:newRequest];
-//            return NO;
-//        }
-//    }
-//    else
-//    {
-//        currentRequest = request;
-//        return YES;
-//    }
-//    
-//    
-//    
-//    
     return YES;
+}
+
+- (void) showBrowser
+{
+    if([self isPresentedPage])
+    {
+        [super showBrowser];
+    }
 }
 
 - (void) hideSubviews
@@ -172,61 +148,13 @@
     
     if(_webView)
     {
-        //[self removeContentFromDisk];
-        
-        //[_webView goBack];
-        //[_webView goBack];
         [_webView loadHTMLString:@"about:blank" baseURL:nil];
         [_webView stopLoading];
         _webView.delegate = nil;
-//        [_webView reload];
         [_webView removeFromSuperview];
-//        [_webView stopLoading];
         _webView = nil;
     }
-    //NSLog(@"cache : %i", [[NSURLCache sharedURLCache] currentMemoryUsage]);
     [[NSURLCache sharedURLCache] removeAllCachedResponses];
-}
-
-
-- (void) removeContentFromDisk
-{
-    PCPageElementHtml *pageElement =(PCPageElementHtml *)[self.page firstElementForType:PCPageElementTypeHtml];
-    
-    NSURL *url = nil;
-    
-    if (pageElement.htmlUrl != nil)
-    {
-        url = [NSURL URLWithString:pageElement.htmlUrl];
-    }
-    else
-    {
-        NSString *archivePath = [[page.revision contentDirectory] stringByAppendingPathComponent:pageElement.resource];
-        
-//        ZipArchive *zipArchive = [[ZipArchive alloc] init];
-//        BOOL openFileResult = [zipArchive UnzipOpenFile:archivePath];
-        
-//        if (openFileResult)
-//        {
-            NSString *outputDirectory = [[archivePath stringByDeletingLastPathComponent] stringByAppendingPathComponent:@"resource"];
-            
-            
-            
-//            [zipArchive UnzipFileTo:outputDirectory overWrite:YES];
-//            url = [NSURL fileURLWithPath:[outputDirectory stringByAppendingPathComponent:@"index.html"]];
-        
-//            [zipArchive UnzipCloseFile];
-        
-        NSError* err = nil;
-        [[NSFileManager defaultManager] removeItemAtPath:outputDirectory error:&err];
-        NSLog(@"%@", err.localizedDescription);
-//        }
-        
-        //[zipArchive release];
-    }
-    
-    
-    
 }
 
 @end
